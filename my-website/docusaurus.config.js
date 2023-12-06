d8b9b592d786f19860c079c49f8fd1d00718a45f
sj-codedemo <141835690+sj-codedemo@users.noreply.github.com>
2023-12-06 11:58:56 +0000

    chore(config): update config entries in docusaurus.config.js
    - modify configuration settings in docusaurus.config.js
    - replace template entries with relevant project information

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -8,20 +8,20 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Simon Jones - Aspiring Software Engineer',
+  tagline: 'Showcasing my Projects, Experience, Skills, and Myself',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://sj-codedemo.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/sj-codedemo.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'sj-demo', // Usually your GitHub org/user name.
+  projectName: 'sj-codedemo.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
